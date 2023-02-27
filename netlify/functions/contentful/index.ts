265414766a48e0facf0e37890b1396f7688b1cee
RyanJoyner <125400865+RyanJoyner@users.noreply.github.com>
2023-02-27 16:25:40 -0700

    Consuming api from useResource$

--- netlify/functions/contentful/index.ts
+++ netlify/functions/contentful/index.ts
@@ -13,17 +13,3 @@ const useGetContentfulEntries: Handler = async (
 };
 
 export { useGetContentfulEntries };
-
-
-// import { Handler } from '@netlify/functions'
-
-// export const handler: Handler = async (event, context) => {
-//   const { name = 'stranger' } = event.queryStringParameters
-
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: `Hello, ${name}!`,
-//     }),
-//   }
-// }
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,21 +1,55 @@
-import { component$, useContextProvider, Slot } from "@builder.io/qwik";
-import { loader$ } from "@builder.io/qwik-city";
+import {
+  component$,
+  useContextProvider,
+  Slot,
+  useResource$,
+} from "@builder.io/qwik";
+// import { loader$ } from "@builder.io/qwik-city";
 // import { useGetContentfulEntries } from "../../netlify/functions/contentful";
 
 import ContentfulContext from "../context/contentful/context";
 
 import Navbar from "../components/navbar/navbar";
 
-export const loadContentfulEntries = loader$(async () => {
-  const response = await fetch("/.netlify/functions/contentful").then(
-    (response) => response.json()
-  );
-  return { data: response.body };
-});
+// export const loadContentfulEntries = loader$(async () => {
+//   const response = await fetch("/.netlify/functions/contentful").then(
+//     (response) => response.json()
+//   );
+//   return { data: response.body };
+// });
+
+export async function getContentfulEntries(
+  controller?: AbortController
+): Promise<any> {
+  const response = await fetch("/.netlify/functions/contentful", {
+    signal: controller?.signal,
+  });
+  // const json = await response.json();
+  // console.log("response", response);
+  // return { data: json.data.items };
+}
 
 export default component$(() => {
-  const signal = loadContentfulEntries();
-  useContextProvider(ContentfulContext, { posts: signal.value.data });
+  // const signal = loadContentfulEntries();
+
+  const contentfulEntries = useResource$<any>(({ track, cleanup }) => {
+    // We need a way to re-run fetching data whenever the `github.org` changes.
+    // Use `track` to trigger re-running of this data fetching function.
+    // track(() => github.org);
+
+    // A good practice is to use `AbortController` to abort the fetching of data if
+    // new request comes in. We create a new `AbortController` and register a `cleanup`
+    // function which is called when this function re-runs.
+    const controller = new AbortController();
+    cleanup(() => controller.abort());
+
+    // Fetch the data and return the promises.
+    return getContentfulEntries(controller);
+  });
+
+  // console.log("contentfulEntries", contentfulEntries);
+
+  // useContextProvider(ContentfulContext, { posts: contentfulEntries });
 
   return (
     <>
