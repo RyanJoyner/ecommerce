64d9ede7e24048d423aec9218abb0ace18efb046
RyanJoyner <125400865+RyanJoyner@users.noreply.github.com>
2023-03-01 08:46:49 -0700

    Added context

---
+++ context/index.tsx
@@ -0,0 +1,22 @@
+import {
+  component$,
+  useSignal,
+  useContextProvider,
+  createContextId,
+} from "@builder.io/qwik";
+
+export type ContentfulContext = any;
+
+// Create a new context descriptor
+export const GlobalContext = createContextId<ContentfulContext>("global");
+
+export default component$(() => {
+  // Creating reactive storage
+  const globalStore = useSignal<ContentfulContext>();
+
+  // Assigning value (state) to the context (ThemeContext)
+  useContextProvider(GlobalContext, globalStore);
+
+  // Not well defined ??
+  return <></>;
+});
